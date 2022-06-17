b1f7b03909c14e300232c19e44028f56efcc8749
DevExpressExampleBot <fakeEmail>
2022-06-17 14:29:17 +0400

    Source auto update [skip ci]

---
+++ VB/CreateAutoHiddenPanels/Window1.xaml.vb
@@ -0,0 +1,16 @@
+Imports System.Windows
+Imports System.Windows.Controls
+
+Namespace CreateAutoHiddenPanels
+
+    ''' <summary>
+    ''' Interaction logic for Window1.xaml
+    ''' </summary>
+    Public Partial Class Window1
+        Inherits Window
+
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
+    End Class
+End Namespace
